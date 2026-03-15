43e10806da58341866eb67d61ea6f96b5974d031
srfashoinnd <srfashionned@gmail.com>
2026-03-15 21:03:45 +0530

    stock system

---
+++ stock_data.js
@@ -0,0 +1,22 @@
+﻿var STOCK_DATA = [
+    {
+        "size":  "M",
+        "stock":  12,
+        "group":  "Shirts",
+        "wholesale":  650,
+        "mrp":  999,
+        "name":  "Cotton Shirt Blue",
+        "barcode":  "890112233",
+        "sale":  799
+    },
+    {
+        "size":  "L",
+        "stock":  6,
+        "group":  "Kurtas",
+        "wholesale":  950,
+        "mrp":  1499,
+        "name":  "Kurta White",
+        "barcode":  "890445566",
+        "sale":  1199
+    }
+]
